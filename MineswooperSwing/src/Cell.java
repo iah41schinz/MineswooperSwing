a43c8d66199deac6dc14085f5b4a57364975df06
schinz <schinz@de-adn-8nzgjx1.adesso.local>
2017-08-03 17:12:43 +0200

    The state of the Cells is now handeled with booleans
    The look of the Icon of the Cell that reflects its current state can now
    be obtained through means of the getStateIcon() method

--- MineswooperSwing/src/Cell.java
+++ MineswooperSwing/src/Cell.java
@@ -4,11 +4,13 @@ import javax.swing.JButton;
 @SuppressWarnings("serial")
 public class Cell extends JButton{
 
-	public boolean Bomb;
+	boolean Bomb,Flagged,Hidden;
 	ImageIcon hidden,flag,bomb,empty,num1,num2,num3,num4,num5,num6,num7,num8;
 	public int neighboringBombs;
 	public Cell() {
 		Bomb = false;
+		Hidden = true;
+		Flagged = false;
 		hidden = new ImageIcon(this.getClass().getResource("hidden.png"));
 		flag = new ImageIcon(this.getClass().getResource("flag.png"));
 		bomb = new ImageIcon(this.getClass().getResource("bomb.png"));
@@ -22,5 +24,42 @@ public class Cell extends JButton{
 		num7 = new ImageIcon(this.getClass().getResource("num7.png"));
 		num8 = new ImageIcon(this.getClass().getResource("num8.png"));
 		this.setIcon(hidden);
+	}
+	
+	public ImageIcon getStateIcon() {
+		if (Flagged == true) {
+			return flag;
+		}
+		else if (Hidden == true) {
+			return hidden;
+		}
+		else if (Bomb == true) {
+			return bomb;
+		}
+		else if(neighboringBombs < 10 && neighboringBombs > 0) {
+			switch(neighboringBombs) {
+			case 1: return num1;
+
+			case 2: return num2;
+
+			case 3: return num3;
+
+			case 4: return num4;
+
+			case 5: return num5;
+
+			case 6: return num6;
+
+			case 7: return num7;
+
+			case 8: return num8;
+
+				default:
+					return empty;
+			}
+		}
+		else {
+			return empty;
+		}
 	}
 }
--- MineswooperSwing/src/Game.java
+++ MineswooperSwing/src/Game.java
@@ -1,3 +1,4 @@
+import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Random;
@@ -15,10 +16,12 @@ public class Game extends JFrame implements MouseListener{
 
 
 	public Game(int x,int y,int numBombs) {
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.numBombs = numBombs;
 		JPanel Panel = new JPanel();
 		this.setSize(400,400);
-		Panel.setSize(400,400);
+		this.setResizable(false);
+		Panel.setLayout(new GridLayout(x,y));
 		Cells = new Cell[y][x];
 		//The Cells are generated
 
@@ -72,81 +75,112 @@ public class Game extends JFrame implements MouseListener{
 		int y = 0;
 		//get the index of the JButton that was pressed
 		if (e.getSource() instanceof Cell) {
-			for (int i = 0; i < Cells.length; i++) {
+Iteration:	for (int i = 0; i < Cells.length; i++) {
 
 				for (int j = 0 ;j < Cells[i].length; j++) {
 					if(e.getSource() == Cells[i][j]) {
 						EventSource = (Cell)e.getSource();
 						x = j;
 						y = i;
+						break Iteration;
 					}
 				}
 			}	
 			if(SwingUtilities.isRightMouseButton(e)) {
-				if(EventSource.getIcon() == EventSource.flag) {
-					EventSource.setIcon(null);
-					if(EventSource.Bomb == true)
+				//Flagging a flagged Cell removes the Flag
+				if(Cells[y][x].Flagged == true) {
+					if(Cells[y][x].Bomb == true) {
 						defusedBombs--;
-				}
-				else if(EventSource.getIcon() == null) {
-					if(EventSource.Bomb == true)
-						defusedBombs++;
-					EventSource.setIcon(EventSource.flag);
-					if(defusedBombs == numBombs) {
-						//TODO Winningscreen
 					}
+					Cells[y][x].Flagged = false;
+				}
+				else if(Cells[y][x].Hidden == true)
+					if(Cells[y][x].Bomb == true) {
+						defusedBombs++;
+					}
+					Cells[y][x].Flagged = true;
+				Cells[y][x].setIcon(Cells[y][x].getStateIcon());
+				if(defusedBombs == numBombs) {
+					//TODO WINSCREEN
 				}
 			}
 			else if(SwingUtilities.isLeftMouseButton(e)) {
-				if(EventSource.Bomb == true) {
-					//TODO Gameoverscreen
+				//Digging a flagged Cell is not possible
+				if(Cells[y][x].Flagged == false) {
+					if(Cells[y][x].Bomb == true) {
+						Cells[y][x].Hidden = false;
+						Cells[y][x].setIcon(Cells[y][x].getStateIcon());
+						//TODO GAMEOVERSCREEN
+					}
+					//Digging an open Cell is not possible
+					if(Cells[y][x].Hidden == true) {
+					openCell(x,y);
+					}
 				}
-				else if(EventSource.getIcon() == null) {
-					OpenCell(y,x);
-				}
-
 
 			}
 		}
 		
 	}
-
-	private void OpenCell(int y, int x) {
-		//Cascade through all neighbor Cells:
-		//First count the bombs around the selected Cell
-		for (int tmpY = y - 1; tmpY < y + 2; tmpY++) {
-xIteration : 	for (int tmpX = x - 1; tmpX < x + 2; tmpX++) {
-				if (tmpX == x && tmpY == y || tmpX < 0 || tmpX > Cells[0].length - 1 || tmpY < 0 || tmpY > Cells.length - 1) {
-					continue xIteration;
-				}
-				else if(Cells[tmpY][tmpX].Bomb == true){
-					Cells[y][x].neighboringBombs++;		
-				}
-				
+//TODO Algorithm is almost working
+	private int openCell(int x, int y) {
+		//check if the coordinates exist
+		if(x >= 0 && x <= Cells[0].length - 1 && y >= 0 && y <= Cells.length - 1) {
+			Cells[y][x].Hidden = false;
+			//if there is a Bomb at (x,y) stop the method
+			if(Cells[y][x].Bomb == true) {
+				return 0;
 			}
 			
-		}
-		//Then, if there are no bombs 
-		//open all surrounding Cells
-		if(Cells[y][x].neighboringBombs == 0) {
+			//Cascade through all neighbor Cells:
+			//First count the bombs around the selected Cell
 			for (int tmpY = y - 1; tmpY < y + 2; tmpY++) {
-				for (int tmpX = x - 1; tmpX < x + 2; tmpX++) {
+	xIteration : 	for (int tmpX = x - 1; tmpX < x + 2; tmpX++) {
 					if (tmpX == x && tmpY == y || tmpX < 0 || tmpX > Cells[0].length - 1 || tmpY < 0 || tmpY > Cells.length - 1) {
-						continue;
+						continue xIteration;
 					}
-					else if(Cells[tmpY][tmpX].getIcon() == null){
-						OpenCell(tmpX, tmpY);
+					else if(Cells[tmpY][tmpX].Bomb == true){
+						Cells[y][x].neighboringBombs++;
 					}
 					
 				}
 				
 			}
+			//Then, if there are no bombs 
+			//open all surrounding Cells
+			if(Cells[y][x].neighboringBombs == 0) {
+				for (int tmpY = y - 1; tmpY < y + 2; tmpY++) {
+					for (int tmpX = x - 1; tmpX < x + 2; tmpX++) {
+						if (tmpX == x && tmpY == y || tmpX < 0 || tmpX > Cells[0].length - 1 || tmpY < 0 || tmpY > Cells.length - 1) {
+							continue;
+						}
+						else if(Cells[tmpY][tmpX].Hidden == true){
+							openCell(tmpX, tmpY);
+						}
+						
+					}
+					
+				}
+			}
+			Cells[y][x].setIcon(Cells[y][x].getStateIcon());
+
 		}
+		return 0;
+	}
+	
+	public void unveilCells() {
+		for (int i = 0; i < Cells.length; i++) {
+
+				for (int j = 0 ;j < Cells[i].length; j++) {
+						Cells[i][j].Hidden = false;
+						Cells[i][j].setIcon(Cells[i][j].getStateIcon());
+
+				}
+		}	
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 }
